aa76d4c0a1a6e9416bf5dd4b750820003d53212d
Rajiv Mandal <90539457+silent-code007@users.noreply.github.com>
2024-05-18 08:53:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-// import categoryRoutes from "./routes/categoryRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
